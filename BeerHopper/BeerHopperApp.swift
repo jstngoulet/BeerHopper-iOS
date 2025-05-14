31210e94e751d9e1c2dca5ee04f3d606e75f6eec
jstngoulet <jstngoulet@me.com>
2025-05-14 12:11:30 -0700

    Initial Commit

---
+++ BeerHopper/BeerHopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeerHopperApp.swift
+//  BeerHopper
+//
+//  Created by Justin Goulet on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct BeerHopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
